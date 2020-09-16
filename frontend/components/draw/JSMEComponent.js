d0b979d0e937dabbfb6ec0a4353b17a52c80f6a4
Raphael Montanari <raphamontana@gmail.com>
2020-09-16 17:21:40 -0300

    Update on JSME

--- frontend/components/draw/JSMEComponent.js
+++ frontend/components/draw/JSMEComponent.js
@@ -45,7 +45,7 @@ function JSMEComponent({ callback }) {
   return(
     <>
       <NonSSRHead>
-        <script type="text/javascript" src="https://peter-ertl.com/jsme/JSME_2017-02-26/jsme/jsme.nocache.js" />
+        <script type="text/javascript" src="https://peter-ertl.com/jsme/JSME_2020-06-11/jsme/jsme.nocache.js" />
       </NonSSRHead>
       <div id={ "jsme_container" } style={{height: '311px'}} />
     </>
